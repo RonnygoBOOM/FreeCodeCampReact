ce63430be466a948c48d32783edb373d1005cb21
Scheibelr <scheibelr@gmail.com>
2022-03-14 13:25:35 -0400

    Tutorial Site with CSS

--- src/components/ContentComponent.js
+++ src/components/ContentComponent.js
@@ -3,18 +3,24 @@ import React from 'react';
 function Content() {
     return (
         <>
-        <h1>Reasons I'm excited to learn react:</h1>
-            <ol>
-                <li>
-                    For my own personal <bold>Growth</bold>
-                </li>
-                <li>    
-                    Creativity
-                </li>
-                <li>
-                    Money
-                </li>
-            </ol>
+            <div className="bg-image">
+                <img src="./logo512.png" alt="half-logo" className="half-logo"></img>
+            </div>
+            <div className="main-content">      
+                <h1>Reasons I'm excited to learn react:</h1> 
+                <ul className="unordered-list">
+
+                    <li>
+                        For my own personal <bold>Growth</bold>
+                    </li>
+                    <li>    
+                        Creativity
+                    </li>
+                    <li>
+                        Career
+                    </li>
+                </ul>
+            </div>  
         </>
     );
 }
